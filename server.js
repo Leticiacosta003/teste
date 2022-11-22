23fdc909572bfb5767490ffe5b858ea1df497ae0
Letícia Costa <113736717+Leticiacosta003@users.noreply.github.com>
2022-11-22 08:37:58 -0300

    Add files via upload
    teste

---
+++ server.js
@@ -0,0 +1,12 @@
+const jsonServer = require('json-server');
+
+const server = jsonServer.create();
+const router = jsonServer.router('db.json'); // <== Sera criado mais tarde
+
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3200; // <== Voce pode alterar a porta
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(port);
